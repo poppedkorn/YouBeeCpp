163796dee86c139c2ca5c870a674b9e480ff2f4b
Jontyae <jontyaeichler@gmail.com>
2023-09-10 22:51:33 +1200

    Finished version of guessing game

--- LO5/rngGuessingGame.cpp
+++ LO5/rngGuessingGame.cpp
@@ -3,52 +3,58 @@
 #include <string>
 using namespace std;
 
+// Potential improvement: Handling floating point numbers
 int main()
 {
-
-    // Providing a seed value
+    // Providing a seed value based on time to generate a unique set of random numbers every instance
     srand((unsigned)time(NULL));
-    int random = rand() % 10 + 1;
-    int inputRandom = random;
+    int lowerLimit = 1;
+    int upperLimit = 10;
+    int random = rand() % upperLimit + lowerLimit;
+    int winningNumber = random;
     int guess;
-    int i;
-    bool win;
+    int guessCounter = 1;
+    string inputString = "";
 
     cout << "          Guess Game"
          << "\n          **********\n"
          << "\nYou will get three chances to guess the number. \n"
-         << "\nGuess a number in between 1 and 10. \n";
+         << "\nGuess a number in between " << lowerLimit << " and " << upperLimit << ". \n";
 
-    // looping the question 3 times.
-    for (int i = 1; i <= 3; i++)
+    while (true)
     {
-        cout << "this is guess number " << i << " : ";
-        cin >> guess;
-        cout << endl;
+        cout << "This is guess #" << guessCounter << ": ";
+        cin >> inputString;
 
-        if (guess == inputRandom)
-        {
-            win = true;
-            cout << "You Won!";
+        try {
+            guess = stoi(inputString);
+        } catch (const std::exception& e) {
+            cout << "Your input was not a number within the range. Try a number between 1-10.\n";
+            continue;
+        }
+
+        if (guess == winningNumber) {
+            cout << "Congratulations, you won! Thanks for playing!";
+            break;
+        } else if (guessCounter == 3) {
+            cout << endl
+                 << "Sorry, wrong guess. Correct number is: " << winningNumber
+                 << endl
+                 << "You have reached the limit! Thanks for playing!";
             break;
         }
-        else
-        {
-            cout << "You Lost!\n";
-        }
-        if (guess >= 11)
-        {
-            cout << "This number is too high!\n";
-        }
-        else if (guess <= 0)
-        {
-            cout << "This number is too low!\n";
-        }
-    }
-    cout << endl
-         << "Correct Number is : " << inputRandom;
-    
 
+        if (guess >= upperLimit + 1) {
+            cout << "Sorry, wrong guess. Your number is bigger than 10. Try a number between 1-10.\n";
+            continue;
+        } else if (guess <= lowerLimit - 1) {
+            cout << "Sorry, wrong guess. Your number is smaller than 1. Try a number between 1-10.\n";
+            continue;
+        } else {
+            cout << "Sorry, wrong guess. Try again!\n";
+            guessCounter++;
+        }   
+    }
 
     return 0;
 }
